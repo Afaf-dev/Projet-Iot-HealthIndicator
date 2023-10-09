f035d5f107ee4d46bafde783960dd221ac66fdc8
HAFSA EL MEKKI <helmekki@indienov.com>
2023-10-09 08:53:05 +0200

    ADD Css and adapt charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { jqxChartComponent } from 'jqwidgets-ng/jqxchart';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
-import { map } from "rxjs"
-import { History } from './models/models'
+import { map } from "rxjs";
+import { History } from './models/models';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,60 @@ import { History } from './models/models'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  @ViewChild('myChart', { static: false }) myChart: jqxChartComponent;
+
   title = 'iot_project';
 
   private historyRef: AngularFireList<History>;
   public historyData: History[] = [];
+  public chartData: any[] = [];
+  public seriesGroups: any[] = [
+    {
+      type: 'line',
+      series: [
+        { dataField: 'Pulsation', displayText: 'Pulsation' }
+      ],
+      xAxis: {
+        dataField: 'Date',
+        type: 'date',
+        baseUnit: 'minute',  // ou 'hour', 'minute' en fonction de la précision souhaitée
+        formatFunction: (value: any) => {
+          return value.toLocaleDateString();  // formatte la date
+        }
+      },
+      valueAxis: {
+        title: { text: 'Pulsation' },
+        minValue: 0  // selon vos besoins
+      }
+    }
+  ];
+  public temperatureChartData: any[] = [];
+  public temperatureSeriesGroups: any[] = [
+    {
+      type: 'line',
+      series: [
+        { dataField: 'Temperature', displayText: 'Temperature' }
+      ],
+      xAxis: {
+        dataField: 'Date',
+        type: 'date',
+        baseUnit: 'day',
+        formatFunction: (value: any) => {
+          return value.toLocaleDateString();
+        }
+      },
+      valueAxis: {
+        title: { text: 'Temperature' },
+        minValue: 0
+      }
+    }
+  ];
 
   constructor(private db: AngularFireDatabase) {
     this.historyRef = db.list<History>('/HealthData');
+    this.historyData.sort((a, b) => {
+      return new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime();
+    });
   }
 
   ngOnInit(): void {
@@ -31,7 +79,35 @@ export class AppComponent implements OnInit {
         )
       )
       .subscribe(data => {
+        console.log("Data from Firebase:", data);
         this.historyData = data;
+        this.historyData.sort((a, b) => {
+          return new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime();
+        });
+        this.updatePulsationChartData();
+        this.updateTemperatureChartData();
       });
+
+
+
   }
+  getLastValue(type: 'temperature' | 'pulsation'): number {
+    const latestData = this.historyData[this.historyData.length - 1]?.data;
+    if (!latestData) return 0;
+    return type === 'temperature' ? latestData.temperature : latestData.pulsation;
+  }
+  updatePulsationChartData(): void {
+    this.chartData = this.historyData.map(h => ({
+      Date: new Date(h.timestamp),
+      Pulsation: h.data.pulsation
+    }));
+  }
+
+  updateTemperatureChartData(): void {
+    this.temperatureChartData = this.historyData.map(h => ({
+      Date: new Date(h.timestamp),
+      Temperature: h.data.temperature
+    }));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,16 @@ import { provideDatabase, getDatabase } from '@angular/fire/database';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
 import { AngularFireModule } from '@angular/fire/compat';
+import { NgChartsModule } from 'ng2-charts';
+import { jqxChartModule } from 'jqwidgets-ng/jqxchart';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    jqxChartModule,
+    NgChartsModule,
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
